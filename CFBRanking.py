cbb9cc9ef16b81edcae652de0fdfe1d0f02e5aa6
Mitchell Boone <Diglett80908@gmail.com>
2025-02-24 13:09:04 -0500

    fixed formatting errors

--- CFBRanking.py
+++ CFBRanking.py
@@ -7,31 +7,29 @@
 # https://docs.google.com/spreadsheets/d/1eLsWt7h0MQLnylBDi_QYnqUgcKCoY78mLvXKkt7vnXM/edit?usp=sharing
 
 
-from io import StringIO
-
-# For retrieving sheet
-import pygsheets as ps
 # For formatting a dataframe into an array
 import numpy as np
+# For retrieving sheet
+import pygsheets as ps
 
 
 # Class for each game played during the season
 class Game:
-    def __init__(self, gameLine):
+    def __init__(self, game_line):
         # Variable setting
-        self.winner = gameLine[0] # Winning team
-        self.loser = gameLine[3] # Losing team
-        self.winnerPts = gameLine[1] # Winning team pts
-        self.loserPts = gameLine[4] # Losing team pts
-        self.winnerLoc = gameLine[2] # location relative to winner
+        self.winner = game_line[0] # Winning team
+        self.loser = game_line[3] # Losing team
+        self.winner_pts = game_line[1] # Winning team pts
+        self.loser_pts = game_line[4] # Losing team pts
+        self.winnerLoc = game_line[2] # location relative to winner
 
         # If no score, sets pts to 0 instead of empty string
-        if self.winnerPts == '':
-            self.winnerPts = 0
-        if self.loserPts == '':
-            self.loserPts = 0
+        if self.winner_pts == '':
+            self.winner_pts = 0
+        if self.loser_pts == '':
+            self.loser_pts = 0
             
-        self.margin = self.winnerPts - self.loserPts # Margin of victory for winner
+        self.margin = self.winner_pts - self.loser_pts # Margin of victory for winner
 
 
 # Class for a team and their schedule and data
@@ -43,11 +41,11 @@ class Team:
 
 # Imports a spreadsheet given the file path, Google sheets key, and sheet name
 # and returns a dataframe
-def importSheet(inFile, inKey, inSheet):
+def import_sheet(in_file, in_key, in_sheet):
     # Generates dataframe
-    access = ps.authorize(service_file=inFile)
-    sheet = access.open_by_key(inKey)
-    tab = sheet.worksheet_by_title(inSheet)
+    access = ps.authorize(service_file=in_file)
+    sheet = access.open_by_key(in_key)
+    tab = sheet.worksheet_by_title(in_sheet)
     df = tab.get_as_df()
     df = df.drop(axis = 1,labels = ["Rk", "Wk", "Date", "Time", "Day", "Notes"])
     df.rename(columns = {"" : "Loc"}, inplace = True)
@@ -58,83 +56,83 @@ def importSheet(inFile, inKey, inSheet):
 
 
 # Parses data from a schedule array to get into a 5 column format and returns the formatted array
-def parseData(inArr):
+def parse_data(in_arr):
 
     # Gets rid of preset rankings in team rows and the trash rows
-    def cleanData(column, cleanArr):
+    def clean_data(column, clean_arr):
         # Counter for current cell
-        count = 0
+        data_count = 0
         
-        for i in cleanArr[:, column]:
+        for _ in clean_arr[:, column]:
             # The current cell being processed
-            cell = cleanArr[count][column]
-            # Adds trash rows to arrDel
-            if cell == "Winner":
-                arrDel.append(count)
+            data_cell = clean_arr[data_count][column]
+            # Adds trash rows to arr_del
+            if data_cell == "Winner":
+                arr_del.append(data_count)
             else:
                 # Cleans up the possible formatting in the team cell
-                if "(" in cell:
-                    cell = cell.replace("(", "")
-                    cell = cell.replace(") ", "")
-                    cell = cell.replace("0", "")
-                    cell = cell.replace("1", "")
-                    cell = cell.replace("2", "")
-                    cell = cell.replace("3", "")
-                    cell = cell.replace("4", "")
-                    cell = cell.replace("5", "")
-                    cell = cell.replace("6", "")
-                    cell = cell.replace("7", "")
-                    cell = cell.replace("8", "")
-                    cell = cell.replace("9", "")
+                if "(" in data_cell:
+                    data_cell = data_cell.replace("(", "")
+                    data_cell = data_cell.replace(") ", "")
+                    data_cell = data_cell.replace("0", "")
+                    data_cell = data_cell.replace("1", "")
+                    data_cell = data_cell.replace("2", "")
+                    data_cell = data_cell.replace("3", "")
+                    data_cell = data_cell.replace("4", "")
+                    data_cell = data_cell.replace("5", "")
+                    data_cell = data_cell.replace("6", "")
+                    data_cell = data_cell.replace("7", "")
+                    data_cell = data_cell.replace("8", "")
+                    data_cell = data_cell.replace("9", "")
                     # For Miami
-                    cell = cell.replace(")", "")
-                    cleanArr[count][column] = cell
+                    data_cell = data_cell.replace(")", "")
+                    clean_arr[data_count][column] = data_cell
 
             # Increment cell counter
-            count += 1
+            data_count += 1
 
         # Returns the array with the team rows cleaned
-        return cleanArr
+        return clean_arr
         
     # The output array
-    outArr = (np.asarray(inArr))
+    out_arr = (np.asarray(in_arr))
 
-    # Array of trash rows containing lables
-    arrDel = []
+    # Array of trash rows containing labels
+    arr_del = []
 
     # Gets rid of preset rankings for both winning and losing teams
-    outArr = cleanData(0, outArr)
-    outArr = cleanData(3, outArr)
+    out_arr = clean_data(0, out_arr)
+    out_arr = clean_data(3, out_arr)
 
-    # Reverse arrDel to avoid index out of bonds error
-    arrDel.reverse()
+    # Reverse arr_del to avoid index out of bonds error
+    arr_del.reverse()
     # Deletes trash rows from arr
-    for i in arrDel:
-        outArr = np.delete(outArr, i, 0)
+    for i in arr_del:
+        out_arr = np.delete(out_arr, i, 0)
 
     # Counter for current cell
-    count = 0
+    cell_count = 0
 
     # Formats the location column
-    for i in outArr[:, 2]:
+    for _ in out_arr[:, 2]:
         # The current cell being processed
-        cell = outArr[count][2]
+        format_cell = out_arr[cell_count][2]
         # Cleans up the possible formatting in the location cell
-        if cell == "":
-            cell = cell.replace("", "H")
-        cell = cell.replace("@", "A")
-        outArr[count][2] = cell
+        if format_cell == "":
+            format_cell = format_cell + "H"
+        format_cell = format_cell.replace("@", "A")
+        out_arr[cell_count][2] = format_cell
         # Increment cell counter
-        count += 1
+        cell_count += 1
 
-    # Returns the the formatted array
-    return outArr
+    # Returns the formatted array
+    return out_arr
 
 
 # creates a list of every team
-def listOfTeams(arr):
+def list_of_teams(arr):
     # List of every team
-    outList = []
+    out_list = []
     # List of every winning team in each game
     list0 = arr[:, 0]
     # List of every losing team in each game
@@ -143,63 +141,62 @@ def listOfTeams(arr):
     list2 = arr[:, 2]
 
     # Counter for use in location checking
-    counter = 0
+    list_counter = 0
     # For each winning team, if the team isn't already in the list of every
     # team and the game was at a neutral site or a home game, add to list
     for  item in list0:
-        if item not in outList and (list2[counter] == ('H' or "N")):
-            outList.append(item)
+        if item not in out_list and (list2[list_counter] == ('H' or "N")):
+            out_list.append(item)
         # increment counter
-        counter += 1
+        list_counter += 1
 
     # Reset counter
-    counter = 0
-    # Counter for use in location checking
-    counter = 0
+    list_counter = 0
+
     # For each losing team, if the team isn't already in the list of every
     # team and the game was at a neutral site or an away game, add to list
     for  item in list3:
-        if item not in outList and (list2[counter] == ('A' or "N")):
-            outList.append(item)
+        if item not in out_list and (list2[list_counter] == ('A' or "N")):
+            out_list.append(item)
         # increment counter
-        counter += 1
+        list_counter += 1
 
     # Returns the list of every team
-    return outList
+    return out_list
 
 
 # Creates a team object
-def createTeam(list1, arr, team):
+def create_team(arr, team):
     # Array for schedule of games
-    scheduleArr = []
+    schedule_arr = []
 
-    # If a game involved the team being created, add the game to scheduleArr
+    # If a game involved the team being created, add the game to schedule_arr
     for i in range(len(arr)):
         if arr[i, 0] == team:
-            gameAdd = Game(arr[i])
-            scheduleArr.append(gameAdd)
+            game_add = Game(arr[i])
+            schedule_arr.append(game_add)
         if arr[i, 3] == team:
-            gameAdd = Game(arr[i])
-            scheduleArr.append(gameAdd)
+            game_add = Game(arr[i])
+            schedule_arr.append(game_add)
 
     # Create a team object using the team's schedule
-    team1 = Team(team, scheduleArr)
+    team1 = Team(team, schedule_arr)
 
     # Return the team object
     return team1
 
 
 # Creates a list of team objects for every team in list
-def createTeamArr(list1, arr):
+def create_team_arr(list1, arr):
     # List of team objects
-    outArr = []
+    out_arr = []
     
     # For every team in the list, create a team object using createTeam
     for item in list1:
-        outArr.append(createTeam(list1, arr, item))
+        out_arr.append(create_team(arr, item))
 
     # Return the array of team objects
-    return outArr
+    return out_arr
 
     
 # Main program
@@ -208,19 +205,19 @@ def main():
     file24 = r"C:/Users/digle/Documents/CFBRanking/cfb-scores-2024-a1167a301572.json"
     key24 = "1eLsWt7h0MQLnylBDi_QYnqUgcKCoY78mLvXKkt7vnXM"
     sheet24 = "Games"
-    df24 = importSheet(file24, key24, sheet24)
-    arr24 = parseData(df24)
+    df24 = import_sheet(file24, key24, sheet24)
+    arr24 = parse_data(df24)
 
     # 2024 list of teams
-    list24 = listOfTeams(arr24)
+    list24 = list_of_teams(arr24)
     list24.sort()
 
     # 2024 array of team objects
-    teamArr24 = createTeamArr(list24, arr24)
+    team_arr24 = create_team_arr(list24, arr24)
 
 
     # List of team objects test
-    for item in teamArr24:
+    for item in team_arr24:
         print(item.name)
         for item2 in item.schedule:
             print('Winner: ' + item2.winner + ' - Loser: ' + item2.loser)
